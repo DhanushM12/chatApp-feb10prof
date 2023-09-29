f19a6592db4eee8b73f64b613b56e96e7c6d75ce
DhanushM12 <dhanushmukhi@gmail.com>
2023-09-29 19:51:58 +0530

    added socket

--- script.js
+++ script.js
@@ -13,11 +13,21 @@ socket.on('user-connected', name => {
     appendMessage(`${name} joined`)
 })
 
+socket.on('chat-message', data => {
+    appendMessage(`${data.name} : ${data.message}`)
+})
+
+socket.on('user-disconnected', name => {
+    appendMessage(`${name} got disconnected`)
+})
+
 messageForm.addEventListener('submit', e => {
     e.preventDefault();
     const message = messageInput.value;
     if(message !== ""){
         appendMessage(`You : ${message}`);
+        socket.emit('send-chat-message', message)
+        messageInput.value = "";
     }
 })
 
